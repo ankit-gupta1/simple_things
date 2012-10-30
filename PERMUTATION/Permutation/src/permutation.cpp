b2f1c253ea7120f8caa62a184c18cbae92e3181d
Ankit Gupta <ankit.ramesh.gupta@gmail.com>
2012-10-30 18:34:41 +0530

    permutation: implementation of permutation of strings.
    All possible permutations of a given string are printed
    using backtracking approach.
    
    Signed-off-by: Ankit Gupta<ankit.ramesh.gupta@gmail.com>

---
+++ PERMUTATION/Permutation/src/permutation.cpp
@@ -0,0 +1,40 @@
+/*
+ * permutation.cpp
+ *
+ *  Created on: 19-Oct-2012
+ *      Author: ankitgupta
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void swap(char &x, char &y)
+{
+	char temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+void permute(char *a, int i, int n)
+{
+   int j;
+   if (i == n)
+     cout<<a<<endl;
+   else {
+        for (j = i; j <= n; j++) {
+          swap(a[i], a[j]);
+          permute(a, i+1, n);
+          swap(a[i], a[j]); //backtrack
+       }
+   }
+}
+
+int main()
+{
+	   char a[] = "ABC";
+	   permute(a, 0, (sizeof(a)/sizeof(a[0])) - 2);
+	   return 0;
+}
+
