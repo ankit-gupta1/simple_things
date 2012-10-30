c3419e69910bc24c0c701608e6b5b54647e010b0
Ankit Gupta <ankit.ramesh.gupta@gmail.com>
2012-10-30 15:11:48 +0530

    helloworld: experimental cpp project.
    Created for experimental purposes.
    
    Signed-off-by: Ankit Gupta <ankit.ramesh.gupta@gmail.com>

---
+++ ABC/ABC/src/main_.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main_.cpp
+// Author      : Ankit Gupta
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!! Hi There" << endl; // prints !!!Hello World!!!
+	return 0;
+}
